bf0daeb54dadeb0c34a8549ed36188e3399ff65e
rones <ronesfinx@gmail.com>
2020-04-29 19:24:20 +0200

    adding new bookmarks to template and retreive it

--- app.py
+++ app.py
@@ -2,13 +2,32 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-user = "Janusz"
+# user = "Janusz"
+bookmarks = [
+    {
+        "user": "Zbigniew",
+        "url": "https://www.ultradesk.pl",
+        "description": "nice IT desk site"
+    },
+    {
+        "user": "Bodzio",
+        "url": "mornigstar.pl",
+        "description": "star stuff"
+    },
+    {
+        "user": "Helena",
+        "url": "https://www.bitbay.net",
+        "description": "coins stuff"
+    }
+]
 
+def get_latest_bookmarks(limit):
+    return bookmarks[:limit]
 
 @app.route("/")
 @app.route("/index")  # dekorator
 def index():
-    return render_template("index.html", user=user)
+    return render_template("index.html", bookmarks=get_latest_bookmarks(2))     # ile mi sie wyswietli bookmarks
 
 
 @app.route("/add")
@@ -17,8 +36,9 @@ def add():
 
 
 @app.errorhandler(404)
-def page_not_founr(e):   #do server not found kasuj e
+def page_not_founr(e):  # do server not found kasuje
     return render_template('404.html'), 404
+
 
 @app.errorhandler(500)
 def server_not_found(e):
@@ -26,4 +46,4 @@ def server_not_found(e):
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
